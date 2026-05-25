c129d57c6ba1ea7e52dc9433890210c86b3a7e3c
Egor Luchiy <egorkaluchiy@proton.me>
2026-05-25 17:05:23 +0300

    Update casino added update

--- handlers/casino_commands.go
+++ handlers/casino_commands.go
@@ -31,6 +31,11 @@ func HandleCasino(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 		return
 	}
 
+	if bet < 50 {
+		bot.Send(tgbotapi.NewMessage(message.Chat.ID, "❌ Минимальная ставка - 50 монет"))
+		return
+	}
+
 	if bet > balance {
 		bot.Send(tgbotapi.NewMessage(message.Chat.ID, fmt.Sprintf("❌ У вас недостаточно монет! Ваш баланс: %d", balance)))
 		return
@@ -83,6 +88,11 @@ func HandleRoulette(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	balance, err := engine.GetBalance(message.From.ID, message.From.UserName)
 	if err != nil {
 		bot.Send(tgbotapi.NewMessage(message.Chat.ID, "❌ Ошибка авторизации игрового профиля."))
+		return
+	}
+
+	if bet < 50 {
+		bot.Send(tgbotapi.NewMessage(message.Chat.ID, "❌ Минимальная ставка - 50 монет"))
 		return
 	}
 
