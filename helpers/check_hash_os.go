9cf266ca2680d57e232fedb2555658c8c6c73f43
Egor Luchiy <egorkaluchiy100@gmail.com>
2026-06-16 18:37:58 +0300

    Update

--- helpers/check_hash_os.go
+++ helpers/check_hash_os.go
@@ -1,43 +1,46 @@
 package helpers
 
-
 import (
+	"errors"
 	"os"
 	"runtime"
-	"errors"
 )
-
 
 func CheckHashAndGpg() error {
 
-    exePath, err := os.Executable()
-    if err != nil {
-        return err
-    }
+	exePath, err := os.Executable()
+	if err != nil {
+		return err
+	}
 
-    switch runtime.GOOS {
-    case "windows":
-        switch runtime.GOARCH {
-        case "amd64", "386":
-            hashFilePath := exePath + ".sha512"
-            expectedHash, err := ReadHashFromFile(hashFilePath)
-            if err != nil {
-                return err
-            }
-            GetFileHashAndVerify(exePath, "sha512", expectedHash)
-            return nil
-        default:
-            return errors.New("Данная архитектура не поддерживается") 
-        }
+	switch runtime.GOOS {
+	case "windows":
+		switch runtime.GOARCH {
+		case "amd64", "386":
+			hashFilePath := exePath + ".sha512"
+			expectedHash, err := ReadHashFromFile(hashFilePath)
+			if err != nil {
+				return err
+			}
+			GetFileHashAndVerify(exePath, "sha512", expectedHash)
+			return nil
+		default:
+			return errors.New("Данная архитектура не поддерживается")
+		}
 
-    case "linux":
-        hashFilePath := exePath + ".asc"
-        if err := VerifyASC(hashFilePath, exePath); err != nil {
-            return errors.New("Ошибка при проверки подписи! Рекомендую: https://github.com/Floren-Team/florenbot/releases")
-        }
-        return nil
+	case "linux":
+		switch runtime.GOARCH {
+		case "amd64", "386", "arm64":
+			ascFilePath := exePath + ".asc"
+			if err := VerifyASC(ascFilePath, exePath); err != nil {
+				return err
+			}
+			return nil
+		default:
+			return errors.New("Данная архитектура не поддерживается")
+		}
 
-    default:
-        return errors.New("Данная ОС не поддерживается")
-    }
+	default:
+		return errors.New("Данная ОС не поддерживается")
+	} 
 }
