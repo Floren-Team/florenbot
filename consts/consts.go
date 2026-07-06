921f4678cea1129b3d4106871095dbf51d460f97
Egor Luchiy <egorkaluchiy100@gmail.com>
2026-07-06 16:05:47 +0300

    Update

--- consts/consts.go
+++ consts/consts.go
@@ -1,8 +1,8 @@
 package consts
 
-const VERSION = "6.0"
+const VERSION = "6.2"
 
-const DATE_LAST_UPDATE = "05.07.2026"
+const DATE_LAST_UPDATE = "06.07.2026"
 
 const OWNER = "Egor Luchiy"
 
--- handlers/chat_commands.go
+++ handlers/chat_commands.go
@@ -52,3 +52,5 @@ func HandleChat(bot *tgbotapi.BotAPI, message *tgbotapi.Message) {
 	}
 	}
 }
+
+
